3eb4376e1edb8d0dec7bcad60aba7aab692859da
Yldrax <eschulz2100@gmail.com>
2025-12-01 17:04:42 +0100

    I hate modulo

--- 2025/python/aoc25_01.py
+++ 2025/python/aoc25_01.py
@@ -2,5 +2,65 @@
 
 from inputs import load_input
 
+
+def part1(instructions: list[str]) -> int:
+    """Turn dial and count occurances of zero"""
+    total = 0
+    dial = 50
+
+    for instruction in instructions:
+        mult = 1 if instruction[0] == "R" else -1
+        step = int(instruction[1:])
+        pointing = (dial + mult * step) % 100
+        if pointing == 0:
+            total += 1
+
+    return total
+
+
+def part2(instructions: list[str]) -> int:
+    """Turn dial and count occurances of passing zero"""
+    total = 0
+    dial = 50
+
+    for instruction in instructions:
+        start = dial
+        mult = 1 if instruction[0] == "R" else -1
+        step = int(instruction[1:])
+        passzero, dial = divmod(dial + mult * step, 100)
+
+        total += abs(passzero)
+
+    return total
+
+
+def part2_force(instructions: list[str]) -> int:
+    dial = 50
+    total = 0
+
+    for instruction in instructions:
+        mult = 1 if instruction[0] == "R" else -1
+        value = int(instruction[1:])
+
+        while value > 0:
+            dial = dial + mult
+            value -= 1
+
+            if dial < 0:
+                dial = 99
+            elif dial > 99:
+                dial = 0
+
+            if dial == 0:
+                total += 1
+
+    return total
+
+
 if __name__ == "__main__":
-    INPUT1 = load_input(1)
+    INPUT = load_input(1).splitlines()
+    print("Part 1: ")
+    print(part1(INPUT))
+    print("Part 2:")
+    print(part2(INPUT))
+    print(part2_force(INPUT))
