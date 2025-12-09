7d16f51e61de624c782854ffa42a46c2bc4f9dd6
Yldrax <eschulz2100@gmail.com>
2025-12-09 14:53:45 +0100

    Formatting

--- 2025/python/aoc25_04.py
+++ 2025/python/aoc25_04.py
@@ -2,5 +2,55 @@
 
 from inputs import load_input
 
+
+def part1(rollmap: list[list[str]]) -> int:
+    total = 0
+
+    for y, row in enumerate(rollmap):
+        for x, roll in enumerate(row):
+            if roll == "@":
+                neighbours = 0
+                offsets = [
+                    (1, -1),
+                    (1, 0),
+                    (1, 1),
+                    (0, -1),
+                    (0, 1),
+                    (-1, -1),
+                    (-1, 0),
+                    (-1, 1),
+                ]
+                for offset in offsets:
+                    try:
+                        neighbours = (
+                            neighbours + 1
+                            if rollmap[y + offset[0]][x + offset[1]] == "@"
+                            else neighbours
+                        )
+                        print(f"coords:{(x, y)}, nc = {(x, y) + offset}")
+                    except IndexError:
+                        pass
+                print(neighbours)
+                if neighbours < 4:
+                    total += 1
+
+    return total
+
+
 if __name__ == "__main__":
-    INPUT = load_input(4)
+    INPUT = [list(line) for line in load_input(4).splitlines()]
+    #       print(part1(INPUT))
+
+    test = """..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+"""
+    test = [list(line) for line in test.splitlines()]
+    print(part1(test))
