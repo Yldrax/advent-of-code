c654982ef9ab25d8c5fca989adbf451603780b8b
Yldrax <eschulz2100@gmail.com>
2025-12-09 15:12:30 +0100

    Day 4 Done

--- 2025/python/aoc25_04.py
+++ 2025/python/aoc25_04.py
@@ -4,53 +4,74 @@ from inputs import load_input
 
 
 def part1(rollmap: list[list[str]]) -> int:
+    """Calculate number of accesible rolls"""
     total = 0
+    height = len(rollmap)
+    width = len(rollmap[0])
 
     for y, row in enumerate(rollmap):
         for x, roll in enumerate(row):
             if roll == "@":
                 neighbours = 0
                 offsets = [
-                    (1, -1),
-                    (1, 0),
-                    (1, 1),
-                    (0, -1),
-                    (0, 1),
                     (-1, -1),
                     (-1, 0),
                     (-1, 1),
+                    (0, -1),
+                    (0, 1),
+                    (1, -1),
+                    (1, 0),
+                    (1, 1),
                 ]
-                for offset in offsets:
-                    try:
-                        neighbours = (
-                            neighbours + 1
-                            if rollmap[y + offset[0]][x + offset[1]] == "@"
-                            else neighbours
-                        )
-                        print(f"coords:{(x, y)}, nc = {(x, y) + offset}")
-                    except IndexError:
-                        pass
-                print(neighbours)
+                for dy, dx in offsets:
+                    ny, nx = y + dy, x + dx
+                    if 0 <= ny < height and 0 <= nx < width:
+                        neighbours += 1 if rollmap[ny][nx] == "@" else 0
                 if neighbours < 4:
                     total += 1
 
     return total
 
 
+def part2(rollmap: list[list[str]]) -> int:
+    "Calculate number of accessible rolls with removal of accesible rolls in the way"
+    total = 0
+    height = len(rollmap)
+    width = len(rollmap[0])
+    change = True
+
+    while change:
+        change = False
+        for y, row in enumerate(rollmap):
+            for x, roll in enumerate(row):
+                if roll == "@":
+                    neighbours = 0
+                    offsets = [
+                        (-1, -1),
+                        (-1, 0),
+                        (-1, 1),
+                        (0, -1),
+                        (0, 1),
+                        (1, -1),
+                        (1, 0),
+                        (1, 1),
+                    ]
+                    for dy, dx in offsets:
+                        ny, nx = y + dy, x + dx
+                        if 0 <= ny < height and 0 <= nx < width:
+                            neighbours += 1 if rollmap[ny][nx] == "@" else 0
+                    if neighbours < 4:
+                        total += 1
+                        rollmap[y][x] = "."
+                        change = True
+
+    return total
+
+
 if __name__ == "__main__":
     INPUT = [list(line) for line in load_input(4).splitlines()]
-    #       print(part1(INPUT))
+    print("Part 1")
+    print(part1(INPUT))
 
-    test = """..@@.@@@@.
-@@@.@.@.@@
-@@@@@.@.@@
-@.@@@@..@.
-@@.@@@@.@@
-.@@@@@@@.@
-.@.@.@.@@@
-@.@@@.@@@@
-.@@@@@@@@.
-@.@.@@@.@.
-"""
-    test = [list(line) for line in test.splitlines()]
-    print(part1(test))
+    print("Part 2")
+    print(part2(INPUT))
