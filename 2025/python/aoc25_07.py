6f26b1694f9b0cb05b0a935c0a98e9e722527e6e
Yldrax <eschulz2100@gmail.com>
2025-12-16 16:59:38 +0100

    Day7 Part 1

--- 2025/python/aoc25_07.py
+++ 2025/python/aoc25_07.py
@@ -2,5 +2,25 @@
 
 from inputs import load_input
 
+
+def part1(manifold: list[list[str]]) -> int:
+    """Simulate tachyon beam and count the splits"""
+    splits = 0
+    manifold[1][len(manifold[0]) // 2] = "|"
+
+    for y in range(1, len(manifold)):
+        for x in range(len(manifold[0])):
+            if manifold[y - 1][x] == "|":
+                if manifold[y][x] == ".":
+                    manifold[y][x] = "|"
+                if manifold[y][x] == "^":
+                    splits += 1
+                    manifold[y][x - 1] = "|"
+                    manifold[y][x + 1] = "|"
+
+    return splits
+
+
 if __name__ == "__main__":
-    INPUT = load_input(7)
+    INPUT1 = [list(line) for line in load_input(7).splitlines()]
+    print(part1(INPUT1))
